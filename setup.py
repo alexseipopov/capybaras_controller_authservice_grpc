ca8bde4596a631e8e20db50d1c3a11a452cea85b
Alexsei Popov <popov2895@gmail.com>
2023-11-04 16:47:46 +0300

    change structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='authservice_grpc',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         # Здесь перечислите зависимости вашего пакета
