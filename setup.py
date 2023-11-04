f026f9aca918724a875ad986c7f44d091eeaad25
Alexsei Popov <popov2895@gmail.com>
2023-11-04 14:19:21 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='authservice_grpc',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Здесь перечислите зависимости вашего пакета
+    ],
+)
