f5e8f34d5ba9b96c11c49bc8d7a7881385e0ef42
Alexsei Popov <popov2895@gmail.com>
2023-11-04 17:08:51 +0300

    change version 4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='authservice_grpc',
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(),
     install_requires=[
         # Здесь перечислите зависимости вашего пакета
